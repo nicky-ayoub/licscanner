83016be99b9e0aeda3ab2658bb3a657757337668
Nicky Ayoub <2052886+nicky-ayoub@users.noreply.github.com>
2025-02-02 13:13:13 -0700

    Started license file scanner with SERVER

--- lib/Lic/Scanner/License.pm
+++ lib/Lic/Scanner/License.pm
@@ -1,7 +1,60 @@
 package Lic::Scanner::License;
 use strict;
 use warnings;
-# ABSTRACT: a really awesome library
+use 5.36.0;
+# ABSTRACT: Flex License Manager File Scanner
+use Lic::Scanner::File;
+
+sub Scan { 
+    my $input = shift;
+    my @lines = Lic::Scanner::File::processBackSlash($input);
+    my $ok = 1;
+    for my $line (@lines) {
+        next if ! $line;
+        #print(STDERR "'$line'\n");
+        if ( $line =~ m{^Server}isxm) {
+            if ( $line =~ s{^(Server)\s+(\S+)\s+(\S+)\s*}{}isxm) {
+                my $host  = $2;
+                my $hostid = $3;
+
+                my $primary_is_master;
+                if ($line =~ s{\bprimary_is_master\b}{}isxm) {
+                    $primary_is_master="primary_is_master"
+                }
+                
+                my $heartbeat_interval ;
+                my $heartbeat_interval_seconds ;
+                if ($line =~ s{\bheartbeat_interval\s*=\s*(\d+)\s*}{}isxm) {
+                    $heartbeat_interval_seconds = 0 + $1;
+                    $heartbeat_interval="HEARTBEAT_INTERVAL";
+                }
+                my $port = "";
+                if ($line =~ s{\b(\d+)\s*}{}isxm) {
+                    $port = 0 + $1;
+                }
+                if ($line !~ m{^\s*$}) {
+                    $ok = 0;
+                    next
+                }
+                
+                $ok = $ok and 1;
+                my $str = "SERVER $host $hostid";
+                $str .= " $port" if $port;
+                $str .= " $primary_is_master" if $primary_is_master;
+                $str .= " $heartbeat_interval=$heartbeat_interval_seconds" if $heartbeat_interval;
+                printf("-i- %s\n", $str);
+            } else {
+                printf ("-e- Server not enough arguments : '%s'\n", $line);
+                $ok = 0;
+            }
+        } else {
+            printf ("-e- Unhandled Option : '%s'\n", $line);
+            $ok = 0;
+        }
+    }
+    # Empty returns 1;
+    return $ok;
+}
 
 1;
 __END__
