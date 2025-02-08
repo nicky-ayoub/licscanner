da4d5ac5152a1fa029dfce89e02ffcdcb71fd5b7
Nicky Ayoub <2052886+nicky-ayoub@users.noreply.github.com>
2025-02-08 15:19:15 -0700

    Refactoring into license command functions. Added Server and Vendor subs

--- lib/Lic/Scanner/License.pm
+++ lib/Lic/Scanner/License.pm
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use 5.36.0;
 use List::UtilsBy qw( extract_by );
+
 # ABSTRACT: Flex License Manager File Scanner
 use Lic::Scanner::File;
 use Lic::Scanner::Chunker;
@@ -18,59 +19,18 @@ sub Scan {
         my @elements = Lic::Scanner::Chunker::chunker($line);
 
         my %attrs;
-        if ( ref $elements[-1] eq ref {}) {
-            %attrs = %{$elements[-1]};
+        if ( ref $elements[-1] eq ref {} ) {
+            %attrs = %{ $elements[-1] };
             pop @elements;
         }
         if ( $elements[0] eq "SERVER" ) {
-            my $command = shift @elements;
-            my $host    = shift @elements;
-            my $hostid = shift @elements;
-            unless ( $host ) {
-                printf( "-e- No Host : '%s'\n", $line );
-                $ok = 0;
-                next;
-            }
-            unless ( $hostid ) {
-                printf( "-e- No Hostid : '%s'\n", $line );
-                $ok = 0;
-                next;
-            }
-            my @removedElements = extract_by { uc() eq 'PRIMARY_IS_MASTER' } @elements;
-            if ( scalar @removedElements > 1 ) {
-                printf( "-e- Duplicate PRIMARY_IS_MASTER : '%s'\n", $line );
-                $ok = 0;
-                next;
-            }
-            my $primary_is_master =  @removedElements;
-            my $port = shift @elements;
-            if ( $port and $port !~ /^\d+$/ ) {
-                printf( "-e- Invalid Port : '%s'\n", $line );
-                $ok = 0;
-                next;
-            }
-            if (@elements) {
-                printf( "-e- Unhandled Options : '%s'\n", $line );
-                $ok = 0;
-                next;
-            }
-            foreach my $key (keys %attrs) {
-                if ( uc($key) eq 'HEARTBEAT_INTERVAL' ) {
-                    my $value = $attrs{$key}->[1];
-                    if ( $value !~ /^\d+$/ ) {
-                        printf( "-e- Invalid HEARTBEAT_INTERVAL value '%d' : '%s'\n", $value, $line );
-                        $ok = 0;
-                        next;
-                    }
-                } else {
-                    print( "-e- Unhandled Option Key '$key': '$line'\n");
-                    $ok = 0;
-                }
-            }
-            $ok = $ok and 1;
+            $ok = $ok && _server( $line, \@elements, \%attrs );
+        }
+        elsif ( $elements[0] eq "VENDOR" ) {
+            $ok = $ok && _vendor( $line, \@elements, \%attrs );;
         }
         elsif ( $elements[0] eq "FEATURE" ) {
-            $ok = $ok and 1;
+            $ok = $ok && 1;
         }
         else {
             printf( "-e- Unhandled Option : '%s'\n", $line );
@@ -80,6 +40,71 @@ sub Scan {
 
     # Empty returns 1;
     return $ok;
+}
+
+sub _server {
+    my $line     = shift;
+    my $elements = shift;
+    my $attrs    = shift;
+
+    my $command  = shift @$elements;
+    my $host     = shift @$elements;
+    my $hostid   = shift @$elements;
+
+    unless ($host) {
+        printf( "-e- No Host : '%s'\n", $line );
+        return 0;
+    }
+    unless ($hostid) {
+        printf( "-e- No Hostid : '%s'\n", $line );
+        return 0;
+    }
+    my @removedElements = extract_by { uc() eq 'PRIMARY_IS_MASTER' } @$elements;
+    if ( scalar @removedElements > 1 ) {
+        printf( "-e- Duplicate PRIMARY_IS_MASTER : '%s'\n", $line );
+        return 0;
+    }
+    my $primary_is_master = @removedElements;
+    my $port              = shift @$elements;
+    if ( $port and $port !~ /^\d+$/ ) {
+        printf( "-e- Invalid Port : '%s'\n", $line );
+        return 0;
+    }
+    if (@$elements) {
+        printf( "-e- Unhandled Options : '%s'\n", $line );
+        return 0;
+    }
+    foreach my $key ( keys %$attrs ) {
+        if ( uc($key) eq 'HEARTBEAT_INTERVAL' ) {
+            my $value = $attrs->{$key}->[1];
+            if ( $value !~ /^\d+$/ ) {
+                printf( "-e- Invalid HEARTBEAT_INTERVAL value '%d' : '%s'\n",
+                    $value, $line );
+                return 0;
+            }
+        }
+        else {
+            print("-e- Unhandled Option Key '$key': '$line'\n");
+            return 0;
+        }
+    }
+    return 1;
+}
+
+sub _vendor {
+    my $line     = shift;
+    my $elements = shift;
+    my $attrs    = shift;
+    
+    my $command  = shift @$elements;
+    my $vendor     = shift @$elements;
+   
+
+    unless ($vendor) {
+        printf( "-e- No Host : '%s'\n", $line );
+        return 0;
+    }
+    return 1;
 }
 
 1;
